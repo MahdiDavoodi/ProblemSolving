3c0e20621d29b6132723a69b42c4b2f0a11c2725
MahdiDavoodi <MahdiDavoodi@gmail.com>
2022-01-23 08:33:24 -0500

    Update - RE

--- LeetCode/RemoveElement/Solution.kt
+++ LeetCode/RemoveElement/Solution.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun removeElement(numbers: IntArray, n: Int): Int {
-        val list = numbers.toMutableList()
-        list.removeAll { x -> x == n }
-        for (i in list.indices) numbers[i] = list[i]
-        return list.size
+        var k = 0
+        for (i in numbers.indices) if (numbers[i] != n) {
+            numbers[k] = numbers[i]
+            k++
+        }
+        return k
     }
 }
-
 /*
-First, convert it to a mutable list. Then remove all the duplicates. Copy the entire list to the first `list.size` elements of `nums`.
-
-Return `list.size` 
+Iterate through the array using two pointers. One of them is pointing at your new array.
+The other one is pointing at your current one. Copy all the numbers except those that are equal to n.
 */
