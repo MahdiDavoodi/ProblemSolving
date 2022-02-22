e94528b071af96523809c0f5b781071bbb673ade
MahdiDavoodi <MahdiDavoodi@gmail.com>
2022-02-22 15:45:58 -0500

    Update - HackerRank - AAD - Iteration 1

--- HackerRank/AppendAndDelete/Iteration1.kt
+++ HackerRank/AppendAndDelete/Iteration1.kt
@@ -1,7 +1,17 @@
-fun main() = println(
-    if (readLine()!! editCount readLine()!! <= readLine()!!.toInt()) "Yes"
-    else "No"
-)
+fun main() {
+    val s = readLine()!!
+    val t = readLine()!!
+    val k = readLine()!!.toInt()
+
+    val minDif = s editCount t
+    println(
+        when {
+            k >= t.length + s.length || (k % 2 == minDif % 2 && k >= minDif) -> "Yes"
+            else -> "No"
+        }
+    )
+}
+
 
 infix fun String.editCount(other: String): Int {
     for (index in 0..maxOf(this.length, other.length)) {
@@ -13,7 +23,5 @@ infix fun String.editCount(other: String): Int {
     return 0
 }
 /**
- * I didn't solve this problem.
- * I just wrote something that passes 12/14 test cases.
- * This is not the solution.
+ * This passes the tests, I don't fucking know why.
  * */
